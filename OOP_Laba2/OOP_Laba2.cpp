708126e74dcea86e2f62bdd56b937a5cec66af78
Oleg <rasskazov-2004@mail.ru>
2024-04-13 14:38:54 +0500

    Заключительный вариант с композицией по объектам (не указателям)

--- OOP_Laba2/OOP_Laba2.cpp
+++ OOP_Laba2/OOP_Laba2.cpp
@@ -72,7 +72,7 @@ public:
     Triangle() : p1(new Point(1,0,0)), p2(new Point(0,1,0)), p3(new Point(0,0,1)) {
         printf("Triangle()\n");
     }
-    Triangle(Point p1, Point p2, Point p3) : p1(new Point(p1)), p2(new Point(p2)), p3(new Point(p3)) {
+    Triangle(Point p1, Point p2, Point p3) : p1(new Point(p1)), p2(new Point(p2)), p3(new Point(p3)) { //p1(&p1)
         printf("Triangle(Point p1, Point p2, Point p3)\n");
     }
     Triangle(const Triangle& t) : p1(t.p1), p2(t.p2), p3(t.p3) {
@@ -96,11 +96,36 @@ public:
     }
 };
 
+class Triangle2 {
+protected:
+    Point p1;
+    Point p2;
+    Point p3;
+public:
+    Triangle2() : p1(Point(1, 0, 0)), p2(Point(0, 1, 0)), p3(Point(0, 0, 1)) {
+        printf("Triangle2()\n");
+    }
+    Triangle2(Point p1, Point p2, Point p3) : p1(p1), p2(p2), p3(p3) {
+        printf("Triangle2(Point p1, Point p2, Point p3)\n");
+    }
+    Triangle2(const Triangle2& t) : p1(t.p1), p2(t.p2), p3(t.p3) {
+        printf("Triangle2(const Triangle& t)\n");
+    }
+    ~Triangle2() {
+        printf("~Triangle2()\n");
+    }
+    void showPoints() {
+        p1.showMe();
+        p2.showMe();
+        p3.showMe();
+    }
+};
+
 class ExamplePoint : public Point {};
 
 int main()
 {
-    Point ps1(1,1,1), ps2(2,2,2), ps3(3,3,3);
+    Point ps1(Point(1,1,1)), ps2(2,2,2), ps3(3,3,3);
     {
     Triangle t(ps1, ps2, ps3); // Здесь ps1, ps2, ps3 передаются "по значению" т.е. создаются копии этих объектов и уже они передаются в констурктор
     // В конструкторе создаются указатели на эти временные объекты
@@ -139,6 +164,7 @@ int main()
     {
         UndefinedPoint* p = new UndefinedPoint(10, 20, 30, 5);
         Point* p2 = new UndefinedPoint(*p);
+        p2->showMe();
         p->defineLocation();
         p2->rLength();
         delete p;
@@ -154,6 +180,32 @@ int main()
         //При поверхностном копировании не создаются новые точки, создаются ссылки на старые точки
         //Поэтому после удаления точек t1 точек t2 не существует, остаются ссылки на место где лежит уже что-то другое
         
+    }
+    printf("Triangle2:\n");
+    {
+        { Triangle2 t1; }
+        printf("--------------\n");
+        {
+        Point p1, p2, p3;
+        Triangle2 t2(p1, p2, p3);
+        }
+        printf("--------------\n");
+        {
+        Point* p4 = new Point(1, 1, 1);
+        Point* p5 = new Point(2, 2, 2);
+        Point* p6 = new Point(3, 3, 3);
+        Triangle2 t3(*p4, *p5, *p6);
+        delete p4;
+        delete p5;
+        delete p6;
+        }
+        printf("--------------\n");
+        {
+        Triangle2* t4 = new Triangle2();
+        Triangle2* t5 = new Triangle2(*t4);
+        delete t4;
+        delete t5;
+        }
     }
     printf("ExamplePoint:\n");
     {
